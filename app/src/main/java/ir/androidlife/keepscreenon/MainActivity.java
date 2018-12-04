feb2736edc12d854531b57d5b601e6c6029fcc40
Korosh1990 <saberikorosh@gmail.com>
2018-12-03 16:59:47 -0800

    Initial commit

---
+++ app/src/main/java/ir/androidlife/keepscreenon/MainActivity.java
@@ -0,0 +1,28 @@
+package ir.androidlife.keepscreenon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Switch screenOnSwitch = findViewById(R.id.screen_on_switch);
+        screenOnSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                } else {
+                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                }
+            }
+        });
+    }
+}
